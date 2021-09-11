8fcd43714e309519732174a94e3b51942cb5b05d
Chris DiPiero <frogmaster99@msn.com>
2021-09-11 11:32:38 -0500

    completed 2.2 + 2.3

--- fso_part2/src/components/Course.js
+++ fso_part2/src/components/Course.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
+
+
 const Course = ({ course }) => {
+  const parts = course.parts
+  const courseSum = parts.reduce(
+    (a, b) => {
+      return a + b.exercises
+    }, 0
+  )
+
   return (
     <div>
       <h1>{course.name}</h1>
       <ul>
-        {course.parts.map( (e) => 
+        {parts.map((e) =>
           <li key={e.id}>{`${e.name}: ${e.exercises}`}</li>
         )}
       </ul>
+      <p>total of {courseSum} exercises</p>
     </div>
   )
 }
