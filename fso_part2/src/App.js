c2307a2976f6f391aa926134ea7fed7608097e78
Chris DiPiero <frogmaster99@msn.com>
2021-09-30 20:44:18 -0500

    added search option

--- fso_part2/src/App.js
+++ fso_part2/src/App.js
@@ -3,10 +3,13 @@ import People from './components/People'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: '123-456-7890'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
+  const [ searchTerm, setSearchTerm ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
@@ -27,25 +30,45 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const searchTermHandler = (event) => {
+    setSearchTerm(event.target.value.toLowerCase())
+  }
+
+  const populateNameList = (e) => <li key={e.name}>{e.name}: {e.number}</li>
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input type='text' value={searchTerm} onChange={searchTermHandler}/>
+      </div>
+      <h2>Add new number</h2>
       <form onSubmit={handleEvent}>
         <div>
-          name: <input type="text" value={newName} onChange={nameUpdateHandler}/>
+          name: <input type='text' value={newName} onChange={nameUpdateHandler}/>
         </div>
         <div>
-          number: <input type="text" value={newNumber} onChange={numberUpdateHandler}/>
+          number: <input type='text' value={newNumber} onChange={numberUpdateHandler}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map( (e) => (
-            <li key={e.name}>{e.name}: {e.number}</li>
-        ))}
+        {
+          persons.map( (e) =>  ( 
+            !searchTerm ? populateNameList(e) : 
+            e.name.toLowerCase().includes(searchTerm) ? populateNameList(e) :
+            null
+          ))
+        //   persons.map( (e) = (
+        //     { e.name.includes(searchTerm) ? (<li key={e.name}>{e.name}: {e.number}</li>) : 
+        //   ))
+          // persons.map( (e) => (
+          //   <li key={e.name}>{e.name}: {e.number}</li>
+          // ))
+        }
       </ul>
     </div>
   )
